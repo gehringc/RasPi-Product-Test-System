88bf3d0ac33d8c77bab3e0f25658f20eec560754
Cam Gehring <gehringc@users.noreply.github.com>
2018-02-10 16:31:48 -0500

    Windows host project files added
    Form1.vb is main file handling UDP communication and windows events

---
+++ UDPdemo/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(50, 74)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(110, 51)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Send Command 1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoCheck = False
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(253, 31)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(119, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Raspberry Pi Active"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoCheck = False
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(253, 74)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(95, 17)
+        Me.CheckBox2.TabIndex = 3
+        Me.CheckBox2.Text = "Arduino Active"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(50, 147)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(110, 51)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Send Command 2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoCheck = False
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(253, 108)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(89, 17)
+        Me.CheckBox3.TabIndex = 4
+        Me.CheckBox3.Text = "Arduino Input"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(253, 147)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Status Log"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(253, 178)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(162, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label2.Location = New System.Drawing.Point(33, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(159, 20)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "I Need a Job in NY"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.Label3.Location = New System.Drawing.Point(90, 33)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "v. 1.0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(446, 252)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ UDPdemo/Form1.vb
@@ -0,0 +1,124 @@
+﻿Imports System.Net.Sockets
+Imports System.Net
+
+Public Class Form1
+
+    Dim dataBytes(2) As Byte
+    Const clientActive As Byte = &H80       '80 xx xx
+    Const rbpiActive As Byte = &H40         '40 xx xx
+    Const arduinoActive As Byte = &H20      '20 xx xx
+    Const arduinoInput As Byte = &H10      '10 xx xx
+    Const commandOne As Byte = 8          'x8 xx xx
+    Const commandTwo As Byte = 4          'x4 xx xx
+    Const piPortName As Int32 = 9999
+    Const piIP As Int64 = 323223523 '&HC0A80003                '192.168.0.3
+    Dim raspberryPi As New IPEndPoint(piIP, piPortName)
+    Delegate Sub SendUDPDel(dataGram() As Byte, length As Byte, destination As IPEndPoint)
+    ' Public pointToSendUDP As New SendUDPDel(AddressOf udpClient.SendAsync)
+    Dim Transport As TimerLayer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Transport = New TimerLayer
+        For i As Byte = 0 To 2
+            dataBytes(i) = &H0
+        Next
+        dataBytes(2) = clientActive                     'Send the data that alerts Raspberry Pi that this program is running
+        Transport.TransportSend(dataBytes)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        dataBytes(2) = dataBytes(2) Xor commandOne        'toggles ccommand 1 bit
+        Transport.TransportSend(dataBytes)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Static flag As Boolean
+        If flag = False Then
+            flag = True
+            TextBox1.Text = "Unblocked"
+        Else
+            flag = False
+            TextBox1.Text = ""
+        End If
+        dataBytes(2) = dataBytes(2) Xor commandTwo        'toggles ccommand 2 bit
+        Transport.TransportSend(dataBytes)
+    End Sub
+
+    Public Sub UpdateGUI(dataGram() As Byte)
+        Dim messageLength = dataGram.Length
+        Dim checkPi As Boolean = dataGram(2) And rbpiActive
+        Dim checkArduino As Boolean = dataGram(2) And arduinoActive
+        Dim arduinoIn As Boolean = dataGram(2) And arduinoInput
+        CheckBox1.Checked = checkPi
+        CheckBox2.Checked = checkArduino
+        CheckBox3.Checked = arduinoIn
+        If checkPi Then
+            TextBox1.Text = dataGram(1).ToString + dataGram(0).ToString
+        End If
+
+    End Sub
+
+
+End Class
+
+
+Public Class UDPClass
+    Dim udpClient As New UdpClient(45557)
+    Dim udpListener As New UdpClient(45555)
+
+    Sub Connect()
+        udpClient.Connect("192.168.0.3", 9999)
+    End Sub
+
+    Sub New()
+
+    End Sub
+
+    Public Sub UDP_Send(data() As Byte)
+        Const dataLength As Byte = 3
+        udpClient.Send(data, dataLength)
+    End Sub
+
+    Public Function ReceiveData() As Byte()
+        Dim receivedData As Byte() = Nothing
+        Dim recieveEndPoint As New IPEndPoint(IPAddress.Any, 0)
+        Try
+            Dim result = udpListener.ReceiveAsync()             'should call in other thread
+            receivedData = result.Result.Buffer
+            recieveEndPoint = result.Result.RemoteEndPoint
+            result.Dispose()
+        Catch
+        End Try
+        Return receivedData
+    End Function
+
+
+End Class
+
+Public Class TimerLayer
+    Dim ApplicationLayer As New System.Windows.Forms.Control()
+    Dim TimerControl As New System.Windows.Forms.Control()
+    Delegate Sub UpdateGuiDel(dataGram() As Byte)
+    Public pointToUpdateGui As New UpdateGuiDel(AddressOf Form1.UpdateGUI)
+    Dim timer2 As New System.Timers.Timer(80)
+    Dim TransportLayer As New UDPClass
+    Dim passingObject As New Object()
+    Sub New()
+        AddHandler timer2.Elapsed, AddressOf OnTimedEvent
+        TransportLayer.Connect()
+        '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''PLEASE TRY
+        timer2.SynchronizingObject = TimerControl
+        timer2.Start()
+        ApplicationLayer.CreateControl()
+    End Sub
+
+    Public Sub OnTimedEvent(source As Object, e As System.Timers.ElapsedEventArgs)
+        Dim result = TransportLayer.ReceiveData()
+        ApplicationLayer.Invoke(pointToUpdateGui, New Object() {result}) 'Remove new object creation?
+    End Sub
+
+    Public Sub TransportSend(data() As Byte)
+        TransportLayer.UDP_Send(data)
+    End Sub
+
+End Class
