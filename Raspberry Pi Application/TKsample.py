1ca33d10a3658842a3ae9cd0100816c3ab18762a
Cam Gehring <gehringc@users.noreply.github.com>
2018-02-10 15:29:05 -0500

    Some comments added

--- Raspberry Pi Application/TKsample.py
+++ Raspberry Pi Application/TKsample.py
@@ -3,7 +3,7 @@ import socket
 import queue
 import threading
 import time
-import wiringpi
+#import wiringpi                    #not yet implemented
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -14,17 +14,19 @@ class Application(tk.Frame):
         self.datas = (0x000040).to_bytes(3,'big')
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+        self.toggler = tk.Button(self)
+        self.toggler["text"] = "Toggle value(click me)"
+        self.toggler["command"] = self.say_hi
+        self.toggler.pack(side="top")
 
-        self.cam_try = tk.Message(self)
+        self.receiveText = tk.Message(self)
         self.intCtrl = tk.IntVar()
-        self.cam_try.pack(side="right")
-        self.cam_try["textvariable"] = self.intCtrl
+        self.receiveText.pack(side="right")
+        self.receiveText["textvariable"] = self.intCtrl
+        
+        #Button that is not yet implemented
         #self.quit = tk.Button(self, text="QUIT", fg="red",
-        #                      command=root.destroy)
+        #                      command=root.destroy)                                    #save for next button implementation
         #self.quit = tk.Button(self, text="Always Update",command=self.updateMessage)
         #self.quit.pack(side="bottom")
 
@@ -34,10 +36,6 @@ class Application(tk.Frame):
         shiftByte = self.flagger << 6
         self.datas = (0x000000 | shiftByte).to_bytes(3,'big')
 
-
-    def start(self):
-        self.updateMessage()
-    
     def updateMessage(self):
             if not q.empty():
                 recData = int.from_bytes(q.get(),'big')
@@ -47,7 +45,7 @@ class Application(tk.Frame):
             q2.join()
             self.after(80, self.updateMessage)
         
-
+#Local IP network set up for UDP communication
 ME_IP = "192.168.0.3"
 ME_PORT = 9999
 recSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -74,17 +72,18 @@ def keepSending():
             sendData = q2.get()
             sender(sendData)
             q2.task_done()
-
-def spiWorker():
-    readCommand = (0x03).to_bytes(1,'big')
-    while True:
-        recvData = wiringpi.wiringPiSPIDataRW(0,readCommand)
-        print(recvData)
+ 
+# SPI data handling via wiringPi not yet implemented
+#def spiWorker():
+#    readCommand = (0x03).to_bytes(1,'big')
+#    while True:
+#        recvData = wiringpi.wiringPiSPIDataRW(0,readCommand)
+#        print(recvData)
         
 q = queue.Queue()
 q2 = queue.LifoQueue()
-wiringpi.wiringPiSetupGpio()
-wiringpi.wiringPiSPISetup(500000,0)
+#wiringpi.wiringPiSetupGpio()               #Not yet implemented
+#wiringpi.wiringPiSPISetup(500000,0)        #Not yet implemented
 root = tk.Tk()
 app = Application(master=root)
 receiveThread = threading.Thread(target=worker)
